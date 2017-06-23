b3b1b8d1fbf788d30d9d221474612f110d8c7e65
Norbert <norbertkozsir@gmail.com>
2017-06-24 00:58:12 +0200

    stuff

--- app/src/main/java/com/fleetmaster/fleetmaster/find_tool_list/recyclerView/ItemAdapter.kt
+++ app/src/main/java/com/fleetmaster/fleetmaster/find_tool_list/recyclerView/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import com.fleetmaster.fleetmaster.R
 
@@ -21,9 +22,31 @@ class ItemAdapter(listData: List<Tool>): RecyclerView.Adapter<ItemAdapter.ViewHo
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
         private val textView = itemView.findViewById(R.id.tool_name) as TextView
+        private val imageButton = itemView.findViewById(R.id.imageButton) as ImageButton
+
+        init {
+            itemView.setOnClickListener {
+
+            }
+
+            imageButton.setOnClickListener {
+
+            }
+        }
 
         fun bindText(name: String) {
             textView.text = name
+        }
+
+        fun bindAvailable(available: Boolean){
+            if(available) {
+                imageButton.setImageResource(R.drawable.ic_check_black_24dp)
+                imageButton.setColorFilter(R.color.green,android.graphics.PorterDuff.Mode.MULTIPLY)
+
+            } else {
+                imageButton.setImageResource(R.drawable.ic_close_black_24dp)
+                imageButton.setColorFilter(R.color.red,android.graphics.PorterDuff.Mode.MULTIPLY)
+            }
         }
     }
 
@@ -37,6 +60,7 @@ class ItemAdapter(listData: List<Tool>): RecyclerView.Adapter<ItemAdapter.ViewHo
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
         holder?.bindText(listData[position].name)
+        holder?.bindAvailable(listData[position].available)
     }
 
 
--- app/src/main/java/com/fleetmaster/fleetmaster/find_tool_list/recyclerView/Tool.kt
+++ app/src/main/java/com/fleetmaster/fleetmaster/find_tool_list/recyclerView/Tool.kt
@@ -3,4 +3,4 @@ package com.fleetmaster.fleetmaster.find_tool_list.recyclerView
 /**
  * Created by Norbert on 23.06.2017.
  */
-class Tool(val name: String, val status: String)
+class Tool(val name: String, val status: String, val available: Boolean)
